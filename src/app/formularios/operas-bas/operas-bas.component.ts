4daeb0c82bebf6e604890d766c47ef828849204a
Angel Moreno <20001427@alumnos.utleon.edu.mx>
2023-09-25 19:34:13 -0600

    25-09-2023 JAME

---
+++ src/app/formularios/operas-bas/operas-bas.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  templateUrl: './operas-bas.component.html',
+  styleUrls: ['./operas-bas.component.css']
+})
+export class OperasBasComponent {
+
+  operacion:string = '';
+  num1:string = '';
+  num2:string = '';
+  resultado:number = 0;
+
+  calcular():void{
+    switch(this.operacion){
+      case 's':
+        this.sumar();
+        break;
+      case 'r':
+        this.restar();
+        break;
+      case 'm':
+        this.multiplicar();
+        break;
+      case 'd':
+        this.dividir();
+        break;
+    }
+  }
+  
+  sumar():void{
+    this.resultado = parseInt(this.num1) + parseInt(this.num2);
+  }
+
+  restar():void{
+    this.resultado = parseInt(this.num1) - parseInt(this.num2);
+  }
+
+  multiplicar():void{
+    this.resultado = parseInt(this.num1) * parseInt(this.num2);
+  }
+
+  dividir():void{
+    this.resultado = parseInt(this.num1) / parseInt(this.num2);
+  }
+
+}
